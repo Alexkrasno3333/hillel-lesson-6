93704570ae3a12b333e14edfb4fd5f6f3558541d
Alexkrasno3333 <goverla123456789@gmail.com>
2025-09-18 20:27:32 +0300

    исправление ошибки при одной цыфре

--- 06/6.3.py
+++ 06/6.3.py
@@ -1,13 +1,24 @@
 tittle = input("Enter your number: ")
+if len(tittle)==1:
+    print(tittle)
+else:
 
-while len(tittle) > 1:
-    product = 1
-    for i in tittle:
-        i = int(i)
-        product *= i
-    tittle = str(product)
+    while len(tittle) > 1:
+        product = 1
+        for i in tittle:
+            i = int(i)
+            product *= i
+        tittle = str(product)
+    print(product)
 
-print(product)
+
+
+
+
+
+
+
+
 
 
 
